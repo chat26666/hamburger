05a10a62ab96c9472c99bdce92b20feba99d5499
Calculator_Java <chat26666@gmail.com>
2025-03-10 18:31:42 +0900

    fix : 유효하지 않은 입력 예외처리

---
+++ src/Kiosk.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
+public class Kiosk {
+    private List<MenuItem> items;
+
+    public Kiosk(String[] name, double[] price, String[] description) {
+        items = new ArrayList<>();
+        for (int i = 0; i < name.length; i++) {
+            items.add(new MenuItem(name[i], price[i], description[i]));
+        }
+    }
+    public void start() {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println("[ SHAKESHACK MENU ]\n");
+            for(int num = 0; num < items.size(); num++) {
+                System.out.println(num+1 +". "+items.get(num).getName()+"    "+"| W "+items.get(num).getPrice()/1000+" | "+items.get(num).getDescription());
+            }
+            System.out.println("0. 종료      | 종료");
+            try {
+                int choice = sc.nextInt();
+                if (choice == 0) {
+                    break;
+                } else if (choice >= 1 && choice <= items.size()) {
+                    System.out.println(items.get(choice - 1).getName() + " 선택\n");
+                } else {
+                    System.out.println("1 ~ "+items.size()+" 선택번호를 입력해주세요.\n");
+                }
+            }
+            catch (InputMismatchException e) {
+                System.out.println("1 ~ "+items.size()+" 선택번호를 입력해주세요.\n");
+                sc.nextLine();
+            }
+        }
+    }
+    public void addMenuItem(MenuItem item) {
+        items.add(item);
+    }
+    public boolean removeMenuItem(String name) {
+        return items.removeIf(item -> item.getName().equals(name));
+    }
+
+    public List<MenuItem> getMenuItems() {
+        return items;
+    }
+    public MenuItem findMenuItem(String name) {
+        for (MenuItem item : items) {
+            if (item.getName().equals(name)) {
+                return item;
+            }
+        }
+        return null;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,14 @@
-import java.util.*;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        List<MenuItem> items = new ArrayList<MenuItem>();
-        items.add(new MenuItem("ShackBurger",6900,"토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
-        items.add(new MenuItem("SmokeShack",8900,"베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
-        items.add(new MenuItem("Cheeseburger",6900,"포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
-        items.add(new MenuItem("Hamburger",5400,"비프패티를 기반으로 야채가 들어간 기본버거"));
-        Scanner sc = new Scanner(System.in);
-        while (true) {
-            System.out.println("[ SHAKESHACK MENU ]\n");
-            for(int num = 0; num < items.size(); num++) {
-                System.out.println(num+1 +". "+items.get(num).getName()+"    "+"| W "+items.get(num).getPrice()/1000+" | "+items.get(num).getDescription());
-            }
-            System.out.println("0. 종료      | 종료");
-            int choice = sc.nextInt();
-            if (choice == 0) {
-                break;
-            }
-        }
+        String[] name = new String[]{"ShackBurger","SmokeShack","Cheeseburger","Hamburger"};
+        double[] price = new double[]{6900, 8900, 6900, 5400};
+        String[] description = new String[]{"토마토, 양상추, 쉑소스가 토핑된 치즈버거","베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거",
+                                            "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거","비프패티를 기반으로 야채가 들어간 기본버거"};
+
+        Kiosk kiosk = new Kiosk(name, price, description);
+        kiosk.start();
+
 
     }
 }
